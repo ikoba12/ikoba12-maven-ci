07032166c5e7a1aa5d175bfaa80cf61fbc89ce46
ikoba12 <ikoba12@freeuni.edu.ge>
2015-04-27 10:23:32 -0800

    step 8

--- ikoba12-maven/src/main/java/HelloWorld.java
+++ ikoba12-maven/src/main/java/HelloWorld.java
@@ -3,4 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
+	public int testMethod(){
+		return -1;
+	}
 }
--- ikoba12-maven/src/test/java/TestClass.java
+++ ikoba12-maven/src/test/java/TestClass.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 
 public class TestClass {
@@ -11,5 +12,11 @@ public class TestClass {
 		int b=0;
 		assertEquals(a, b);
 	}
-
+	@Test
+	public void mockTest(){
+		HelloWorld test=Mockito.mock(HelloWorld.class);
+		Mockito.when(test.testMethod()).thenReturn(100);
+		assertEquals(100, test.testMethod());
+		
+	}
 }
