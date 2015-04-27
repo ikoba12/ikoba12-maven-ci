a521ef08140112a6a14383c23e533cc1961deec3
ikoba12 <ikoba12@freeuni.edu.ge>
2015-04-27 09:47:27 -0800

    step 7

---
+++ ikoba12-maven/src/test/java/TestClass.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TestClass {
+
+	@Test
+	public void simpleTest() {
+		int a=0;
+		int b=0;
+		assertEquals(a, b);
+	}
+
+}
